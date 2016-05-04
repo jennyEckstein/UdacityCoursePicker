1e3ec56e521b4bcdb68147d7ee65a11efcc358ad
Jenny <ycharnetskaya@gmail.com>
2016-05-03 22:48:49 -0400

    feat: added basic detail activity fragment item feat: now you can go to detail view by clicking on a list item

--- app/src/main/java/com/jennyeckstein/udacitycoursepicker/DetailActivityFragment.java
+++ app/src/main/java/com/jennyeckstein/udacitycoursepicker/DetailActivityFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,12 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.detail_course_image);
+        Picasso.with(getContext()).load(R.drawable.course_test_image).into(imageView);
+
+        return view;
     }
 }
--- app/src/main/java/com/jennyeckstein/udacitycoursepicker/MainActivityFragment.java
+++ app/src/main/java/com/jennyeckstein/udacitycoursepicker/MainActivityFragment.java
@@ -1,11 +1,13 @@
 package com.jennyeckstein.udacitycoursepicker;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 /**
@@ -23,6 +25,14 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         ListView listView = (ListView) view.findViewById(R.id.courseView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Log.v(LOG_TAG, "Start Adapter");
         CourseAdapter courseAdapter = new CourseAdapter(getContext(), getActivity());
