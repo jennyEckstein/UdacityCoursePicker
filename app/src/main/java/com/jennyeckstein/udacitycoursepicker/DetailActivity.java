cad7d04aebe74c3f9a56f6e32cc2365ef8439c33
Jenny <ycharnetskaya@gmail.com>
2016-05-07 14:50:56 -0400

    feat: added share intent to the DetailActivity so the course can be shared with the world

--- app/src/main/java/com/jennyeckstein/udacitycoursepicker/DetailActivity.java
+++ app/src/main/java/com/jennyeckstein/udacitycoursepicker/DetailActivity.java
@@ -1,16 +1,22 @@
 package com.jennyeckstein.udacitycoursepicker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ActionProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private ActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,4 +40,31 @@ public class DetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }else if(id == R.id.action_share){
+            //TODO: in the future it should share course title and other useful info
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Awesome Course #UdacityCoursePicker");
+            shareIntent.setType("text/plain");
+            startActivity(shareIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
