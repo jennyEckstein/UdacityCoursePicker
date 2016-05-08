c803e39d6c3545df48d0d1cc54e54bbff957cde3
Jenny <ycharnetskaya@gmail.com>
2016-05-08 19:10:01 -0400

    task: types out all the json values to parse

--- app/src/main/java/com/jennyeckstein/udacitycoursepicker/UdacityContentAsyncTask.java
+++ app/src/main/java/com/jennyeckstein/udacitycoursepicker/UdacityContentAsyncTask.java
@@ -75,8 +75,41 @@ public class UdacityContentAsyncTask extends AsyncTask {
 
     private void parseJsonString(String jsonString){
         final String COURSES = "courses";
+        final String INSTRUCTORS = "instructors";
+            final String BIO = "bio";
+            //final String IMAGE = "image";
+            final String NAME = "name";
+        final String SUBTITLE = "subtitle";
         final String KEY = "key";
+        final String IMAGE = "image";
+        final String EXPECTED_LEARNING = "expected_learning";
+        final String FEATURED = "featured";
+        final String PROJECT_NAME = "project_name";
+        final String TEASER_VIDEO = "teaser_video";
+            final String YOUTUBE_URL = "youtube_url";
         final String TITLE = "title";
+        final String RELATED_DEGREE_KEYS = "related_degree_keys"; //array
+        final String REQUIRED_KNOWLEDGE = "required_knowledge";
+        final String SYLLABUS = "syllabus";
+        final String NEW_RELEASE = "new_release";
+        final String HOMEPAGE = "homepage";
+        final String PROJECT_DESCRIPTION = "project_description";
+        final String FULL_COURSE_AVAILABLE = "full_course_available";
+        final String FAQ = "faq";
+        final String AFFILIATES = "affiliates"; //array
+        final String TRACKS = "tracks"; //array
+        final String BANNER_IMAGE = "banner_image";
+        final String SHORT_SUMMARY = "short_summary";
+        final String SLUG = "slug";
+        final String STARTER = "starter";
+        final String LEVEL = "level";
+        final String EXPECTED_DURATION_UNIT = "expected_duration_unit";
+        final String SUMMARY = "summary";
+        final String EXPECTED_DURATION = "expected_duration";
+
+
+
+
 
         try{
             JSONObject courseObject  = new JSONObject(jsonString);
@@ -86,6 +119,8 @@ public class UdacityContentAsyncTask extends AsyncTask {
                 JSONObject object = jsonArray.getJSONObject(i);
                 String key = object.getString(KEY);
                 String title = object.getString(TITLE);
+                String subtitle = object.getString(SUBTITLE);
+
                 Log.v(LOG_TAG, key + " - " + title);
             }
 
