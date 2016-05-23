7488a2c8cc705ff63fc7de62c2f1c2177eb5db0e
Jenny <ycharnetskaya@gmail.com>
2016-05-23 10:46:12 -0400

    feat: added interval for the sync adapter

--- app/src/main/java/com/jennyeckstein/udacitycoursepicker/MainActivity.java
+++ app/src/main/java/com/jennyeckstein/udacitycoursepicker/MainActivity.java
@@ -27,8 +27,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //UdacityContentAsyncTask loadData = new UdacityContentAsyncTask(getApplicationContext());
-       // loadData.execute();
+
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
