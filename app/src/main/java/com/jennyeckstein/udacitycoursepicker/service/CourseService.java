78df8599f28e7bb5fde10bc93652d4c1a1d65b9e
Jenny <ycharnetskaya@gmail.com>
2016-05-23 09:20:56 -0400

    task: added class for Service to improve data download

---
+++ app/src/main/java/com/jennyeckstein/udacitycoursepicker/service/CourseService.java
@@ -0,0 +1,17 @@
+package com.jennyeckstein.udacitycoursepicker.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Jenny on 5/23/2016.
+ */
+public class CourseService extends IntentService {
+
+    public CourseService(){super("CourseService");}
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
